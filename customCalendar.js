a1b025ccf053f4c867ac054158709dcee5248a0e
Alessandro018 <alessandrosilva325@gmail.com>
2021-08-08 12:25:16 -0300

    Days selection

--- customCalendar.js
+++ customCalendar.js
@@ -1,4 +1,25 @@
-    document.querySelector(".list-days .day").addEventListener('click', (event) => {
-        console.log(event)
-    }) 
-    
+window.onload = () => {
+    const listDays = document.querySelector(".list-days")
+    const daysOfTheWeek = [
+        "Dom",
+        "Seg",
+        "Ter",
+        "Qua",
+        "Qui",
+        "Sex",
+        "Sáb"
+    ];
+    for(let weekday=0; weekday < daysOfTheWeek.length; weekday++) {
+        listDays.innerHTML += `<div class="weekday">${daysOfTheWeek[weekday]}</div>`;
+    }
+    for(let i=1; i<=31; i++){
+        listDays.innerHTML += `<div class="day" data-day="${i}">${i}</div>`;
+    }
+
+    const days = document.querySelectorAll(".list-days .day")
+    for (let i = 0; i < days.length; i++) {
+        days[i].addEventListener('click', () => {
+            days[i].classList.toggle("selected-day")
+        })
+    } 
+}
