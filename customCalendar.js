7ca30312085a1238f6944e88a1cdbe1abfcd7a26
Alessandro018 <alessandrosilva325@gmail.com>
2021-08-10 22:49:17 -0300

    Move to the previous month and next month

--- customCalendar.js
+++ customCalendar.js
@@ -1,7 +1,4 @@
-window.onload = () => {
-    const currentYear = new Date().getFullYear()
-    const Calendar = (date = null, listEventDays = []) => {
-        date = date ?? new Date();
+    const Calendar = (date = new Date(), minimumYear = 5, listEventDays = []) => {
         const listMonths = document.querySelector(".list-months")
         const listDays = document.querySelector(".list-days")
         const currentMonth = date.getMonth();
@@ -31,9 +28,13 @@ window.onload = () => {
             "Dezembro"
         ]
         
+        document.getElementById("month").setAttribute("data-month", currentMonth)
         document.getElementById("month").innerHTML = months[currentMonth]
         listDays.innerHTML = ""
         listMonths.innerHTML = ""
+        if(document.getElementById("year")) {
+            document.getElementById("year").innerHTML = ""
+        }
         for (let month = 0; month < months.length; month++) {
             let getMonth = month.length === 1 ? `0${month}` : month;
             listMonths.innerHTML += `
@@ -55,10 +56,22 @@ window.onload = () => {
                 ${i}
                 ${listEventDays.indexOf(i) >= 0 ? '<div class="event"></div>' : ''}
             </div>`;
-        }    
+        }
+
+        if(document.getElementById("year")) {
+            if(date.getMonth() === 11) {
+                document.getElementById("year").innerHTML += `
+                <option value="${date.getFullYear() + 1}">${date.getFullYear() + 1}</option>`
+            }
+            for (let i = 0; i < minimumYear; i++) {
+                document.getElementById("year").innerHTML += `
+                <option value="${date.getFullYear() - i}" ${date.getFullYear() - i === date.getFullYear() ? 'selected' : ''}>
+                    ${date.getFullYear() - i}
+                </option>`
+            }
+        }
+         
     }
-    
-    Calendar();
     
     const selectDay = (element) => {
         element.classList.toggle("selected-day")
@@ -66,12 +79,24 @@ window.onload = () => {
 
     const selectMonth =  (element) => {
         let selectedMonth = element.getAttribute("data-month")
+        let yearSelected = document.getElementById("year") ? document.getElementById("year").value : new Date().getFullYear()
         document.getElementsByClassName("selected-month")[0]?.classList.remove("selected-month")
         element.classList.toggle("selected-month")
         document.getElementById("months").style.display = "none"
         document.getElementById("calendar-content").style.display = "flex"
         document.getElementById("month").innerHTML = element.innerHTML
-        Calendar(new Date(currentYear, selectedMonth))
+        Calendar(new Date(yearSelected, selectedMonth))
+    }
+
+    const changeMonth = (element) => {
+        let selectedMonth = parseInt(document.getElementById("month").getAttribute("data-month"))
+        let yearSelected = document.getElementById("year") ? document.getElementById("year").value : new Date().getFullYear()
+
+        if(/^(\+)?([0-9]+)$/.test(selectedMonth)){
+            let month = (element.classList[1] == "next-month") ? selectedMonth + 1 : selectedMonth -1;
+            Calendar(new Date(yearSelected, month))
+        }
+
     }
 
     document.getElementById("month").onclick = () => {
@@ -80,8 +105,8 @@ window.onload = () => {
     }
 
     document.querySelector("body").addEventListener("click", (event) => {
-        let getElementClass = event.target.getAttribute("class")?.split(" ")
-        let elementClass = getElementClass ? getElementClass[0] : null;
+        let getElementClass = event.target.classList
+        let elementClass = getElementClass[0] ? getElementClass[0] : null;
 
         switch(elementClass){
             case "day":
@@ -90,6 +115,8 @@ window.onload = () => {
             case "month":
                 selectMonth(event.target)
                 break;
+            case "change-month":
+                changeMonth(event.target)
+                break;
         }
     })
-}
